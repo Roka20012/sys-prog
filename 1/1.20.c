6dd7b505b22b0377d79130cf3c580dd189a54e7a
Roka20012 <broslavsky.rostislaw@gmail.com>
2021-01-20 08:29:32 +0200

    1.20 ✅

---
+++ 1/1.20.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand((unsigned int)time(NULL));
+    int n;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    printf("%f ", (n * ((float)rand() / RAND_MAX)));
+
+    return 0;
+}
